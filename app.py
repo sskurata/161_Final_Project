5b6691c0044e116a84c04d8237e9115bb1a64da9
sskurata <sskurata@up.edu.ph>
2023-06-26 16:40:30 +0800

    Making files

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import pets
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """<h1>Adopt a Pet!</h1>
-                <p>Browse through the links below to find your new furry friend:</p>
-                <ul>
-                    <li><a href="/animals/dogs">Dogs</a></li>
-                    <li><a href="/animals/cats">Cats</a></li>
-                    <li><a href="/animals/rabbits">Rabbits</a></li>
-                </ul>
-            """
+    return render_template('index.html'))
 
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
